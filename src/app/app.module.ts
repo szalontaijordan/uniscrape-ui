4cd1b2f93e311ba1444e908000ac9dc7926122c2
Szalontai Jordán <jordanlt1111@gmail.com>
2018-12-28 19:05:35 +0100

    imported http module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -27,7 +28,8 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgReduxModule
+    NgReduxModule,
+    HttpClientModule
   ],
   providers: [AuthEpics, AuthActions],
   bootstrap: [AppComponent]
